3c61ca71815c831bc9d3fd46b74e82e62ccdc097
Izabela Toledo <it.amancio@gmail.com>
2020-12-29 16:16:02 -0300

    happy holidays

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 import Countdown from "./countdown.js"
 
-const timeForXtmas = new Countdown("25 December 2020 00:00:00 GMT-0300")
+const timeForNewYear = new Countdown("31 December 2020 00:00:00 GMT-0300")
 
 
 function showTime() {
   const tempos = document.querySelectorAll('[data-time]')
   tempos.forEach((tempo, index) => {
-    tempo.innerHTML = timeForXtmas.total[index]
+    tempo.innerHTML = timeForNewYear.total[index]
 }) 
 }
 
